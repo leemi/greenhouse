d892f0d777ede7b613334c1ffc4d2c200c957c08
Michael Lee <michael.lee@autodesk.com>
2018-12-17 15:34:07 -0500

    feat: command line options

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+/*
+A specificity of indoor plant growing with grow lights is that you need to figure out what
+the light cycle for the plants will be, as there won’t be sun that dictates when plants
+get the light. Light cycles are used to simulate the conditions of day and night for indoor
+gardens and you need to know your light cycles to grow your succulents faster.
+A general rule of thumb for growing any plants under lights is that the more light you give
+them, the faster they will grow and the same can be attributed to succulents. If you are
+overwintering the plants, then I would put the succulents at first on 20/4 light cycle
+meaning that I would leave the lights on for 20 hours a day and then turn them off for
+4 hours and slowly increase the darkness period so the light cycle is 16/8 (light/dark).
+Succulents need to know when it is winter, so they can start their dormant process.
+If you use grow lights all year around for your succulents, during the summer you can
+have them on 24/0 or 20/4 light cycle for them to grow quickly and thrive.
+ */
+
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-
+var readline = require('readline');
 var schedule = require('node-schedule');
 
 const port = new SerialPort("COM5", {
@@ -12,14 +27,22 @@ parser.on('data', function(data) {
   console.log('<', data);
 });
 
-schedule.scheduleJob('0 14 * * *', function() {
-  console.log('> lights on');
+schedule.scheduleJob('0 8 * * *', function() {
   port.write('lights on');
 });
 
-schedule.scheduleJob('0 10 * * *', function() {
-  console.log('> lights off');
+schedule.scheduleJob('0 0 * * *', function() {
   port.write('lights off');
+});
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line){
+  console.log('>', line);
+  port.write(line);
 });
 
 setInterval(() => {}, 1 << 30);
--- lights.ino
+++ lights.ino
@@ -1,16 +1,52 @@
+#ifdef LIGHT_MODULE
+
 #include <FastLED.h>
 
 #define NUM_LEDS 60
-#define MAX_BRIGHTNESS  255
-#define MIN_BRIGHTNESS  64
 #define LED_PIN 6
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
+int brightness = 0;
+bool topOn = false;
+String colorMode = "grow";
+CRGB color = CRGB::White;
+
 void setup() { 
   Serial.begin(9600);
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+}
+
+void disableTopLEDs() {
+  leds[59] = CRGB::Black;
+  leds[58] = CRGB::Black;
+  leds[57] = CRGB::Black;
+  leds[56] = CRGB::Black;
+
+  leds[44] = CRGB::Black;
+  leds[43] = CRGB::Black;
+  leds[42] = CRGB::Black;
+  leds[41] = CRGB::Black;
+  leds[40] = CRGB::Black;
+  leds[39] = CRGB::Black;
+  leds[38] = CRGB::Black;
+  leds[37] = CRGB::Black;
+  leds[36] = CRGB::Black;
+  leds[35] = CRGB::Black;
+  leds[34] = CRGB::Black;
+
+  leds[22] = CRGB::Black;
+  leds[21] = CRGB::Black;
+  leds[20] = CRGB::Black;
+  leds[19] = CRGB::Black;
+  leds[18] = CRGB::Black;
+  leds[17] = CRGB::Black;
+  leds[16] = CRGB::Black;
+  leds[15] = CRGB::Black;
+  leds[14] = CRGB::Black;
+  leds[13] = CRGB::Black;
+  leds[12] = CRGB::Black;
 }
 
 void growColors() {
@@ -21,26 +57,81 @@ void growColors() {
       leds[i] = CRGB::Blue;
     }
   }
+}
+
+void customColors() {
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = color;
+  }
+}
+
+void updateLights() {
+  FastLED.setBrightness(brightness);
+  if (colorMode == "grow") {
+    growColors();  
+  } else if (colorMode == "rgb") {
+    customColors();
+  }
+  if (!topOn) {
+    disableTopLEDs();
+  }
   FastLED.show();
 }
 
-void toggleLights(bool show) {
-  if (show) {
-    FastLED.setBrightness(MAX_BRIGHTNESS);
-  } else {
-    FastLED.setBrightness(0);
-  }
-  growColors();
+void printStatus() {
+  Serial.print("Brightness: ");
+  Serial.println(brightness);
+  Serial.println("Color Mode: " + colorMode);
+  Serial.print("Top: ");
+  Serial.println(topOn ? "on" : "off");
+}
+
+void printHelp() {
+  Serial.println("Commands: ");
+  Serial.println("lights < on | full | dim | off >");
+  Serial.println("lights brightness <0-255>");
+  Serial.println("lights color < white | grow >");
+  Serial.println("lights color <0-255> <0-255> <0-255>");
+  Serial.println("lights < all | bottom >");
 }
 
 void loop() { 
   while(Serial.available()) {
     String a = Serial.readString();
+    a.trim();
     Serial.println(a);
-    if (a == "lights on") {
-      toggleLights(true);
+    if (a == "status") {
+      printStatus();
+    } else if (a == "help") {
+      printHelp();
+    } else  if (a == "lights on" || a == "lights full") {
+      brightness = 255;
     } else if (a == "lights off") {
-      toggleLights(false);
+      brightness = 0;
+    } else if (a == "lights dim") {
+      brightness = 128;
+    } else if (a == "lights color white") {
+      colorMode = "rgb";
+      color = CRGB(255, 255, 255);
+    } else if (a == "lights color grow") {
+      colorMode = "grow";
+    } else if (a == "lights all") {
+      topOn = true;
+    } else if (a == "lights bottom") {
+      topOn = false;
+    } else if (a.startsWith("lights brightness ")) {
+      brightness = a.substring(18).toInt();
+    } else if (a.startsWith("lights color ")) {
+      String c = a.substring(13);
+      int r = c.substring(0, c.indexOf(' ')).toInt();
+      int g = c.substring(c.indexOf(' ') + 1, c.lastIndexOf(' ')).toInt();
+      int b = c.substring(c.lastIndexOf(' ') + 1).toInt();
+
+      colorMode = "rgb";
+      color = CRGB(r, g, b);
     }
+    updateLights();
   }
 }
+
+#endif
